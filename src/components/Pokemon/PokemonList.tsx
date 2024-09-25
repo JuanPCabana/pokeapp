9832a7c54a7171968a8eed2d49aafebc3c14dee1
juanpcabana <juanpx99@gmail.com>
2024-09-25 03:36:52 +0200

    manejo de breakpoints y manejo de array de pokemones vacio

--- src/components/Pokemon/PokemonList.tsx
+++ src/components/Pokemon/PokemonList.tsx
@@ -7,10 +7,19 @@ import { useAppSelector } from '@/redux/hooks'
 const PokemonList = () => {
   const pokemonList = useAppSelector((state) => state.pokemonReducer.displayPokemonList)
 
+  if (pokemonList.length === 0) return (
+    <div className='flex justify-center items-center w-full h-full'>
+      <p className='text-2xl text-gray-500'>No se encontraron pokemones</p>
+    </div>
+  )
+
   return (
-    <div className='grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 2xl:grid-cols-6 gap-4 min-h-4/5 min-w-full p-8'>
+
+    <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4 min-h-4/5 min-w-full p-8'>
       {pokemonList.map((pokemon) => <PokemonCard key={pokemon.name} pokemon={pokemon} />)}
     </div>
+
+
   )
 }
 
